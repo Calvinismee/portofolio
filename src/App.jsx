285c44d34aae49582b00559d00e4481908be1a0b
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-08-16 19:57:51 +0700

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="card">
+      <p className="read-the-docs">
+        Balls
+      </p>
+    </div>
+  )
+}
+
+export default App
